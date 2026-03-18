9fcae225e9faae4dd749c7a2d6fa45e6c5111328
Michael Murray <murraygf35@gmail.com>
2026-03-18 15:01:08 -0400

    fix: add 144x144 icon size for Android WebAPK

--- client/vite.config.js
+++ client/vite.config.js
@@ -24,6 +24,12 @@ export default defineConfig({
             purpose: "any",
           },
           {
+            src: "/icon-192.png",
+            sizes: "144x144",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
             src: "/icon-512.png",
             sizes: "512x512",
             type: "image/png",
