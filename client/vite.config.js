afeb71addc5d7ee3ed69a02d9877d0b6d5cc4269
Michael Murray <murraygf35@gmail.com>
2026-03-18 14:59:04 -0400

    fix: explicit icon purpose and manifest v3

--- client/vite.config.js
+++ client/vite.config.js
@@ -21,11 +21,13 @@ export default defineConfig({
             src: "/icon-192.png",
             sizes: "192x192",
             type: "image/png",
+            purpose: "any",
           },
           {
             src: "/icon-512.png",
             sizes: "512x512",
             type: "image/png",
+            purpose: "any",
           },
         ],
       },
